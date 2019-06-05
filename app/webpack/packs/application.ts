68951d47fc8108458a93fee859b5f529caaae548
Alfonso Valencia <alfonsitovale@gmail.com>
2019-06-04 22:41:19 -0500

    Initial commit
    Configured rails project, using webpacker, react, typescript and bootstrap

---
+++ app/webpack/packs/application.ts
@@ -0,0 +1,11 @@
+//Styles
+import './styles';
+
+// TypeScript
+import Rails from 'rails-ujs';
+import Turbolinks from 'turbolinks';
+
+Rails.start();
+Turbolinks.start();
+
+console.log('Hello world from webpacker');
